a969af60c41c076ade0ae94e71b9026e5b48e3c5
Sajedul2500 <si.sifat2500@gmail.com>
2022-04-07 01:47:34 +0600

    Form

--- index.php
+++ index.php
@@ -3,17 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=<, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
 </head>
 <body>
-    <form action="" method="GET">
-        <label>Username</label><br>
-        <input type="text"><br>
-        <label>Email</label><br>
-        <input type="email"><br>
-        <label>Password</label><br>
-        <input type="password"><br>
-    </form>
+    
 </body>
 </html>
